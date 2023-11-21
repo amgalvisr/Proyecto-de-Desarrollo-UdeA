87687ef105547891e09e038a211146ac901e9502
amgalvisr <88796157+amgalvisr@users.noreply.github.com>
2023-11-20 21:52:13 -0500

    Delete Grupo7 directory

--- Grupo7/Grupo7/Grupo7/src/main/java/ProyectoDesarrolloUdea/Grupo7/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ProyectoDesarrolloUdea.Grupo7.repository;
-
-import ProyectoDesarrolloUdea.Grupo7.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByEmail(String email);
-}
